2f85a1645c861b4e7cc9aa6f841f3c98bab180bc
JTBallew <jtballew06@gmai.com>
2025-07-29 20:50:20 -0500

    Added buttons for 4 different towers

--- Assets/Scripts/Manager/TowerPlaceManager.cs
+++ Assets/Scripts/Manager/TowerPlaceManager.cs
@@ -1,7 +1,13 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TowerPlaceManager : MonoBehaviour
 {
+    public Camera MainCamera;
+    public LayerMask TileLayer;
+    public InputAction PlaceTowerAction;
+
+    [SerializeField] private bool isPlacingTowwer = false;
 
     void Start()
     {
@@ -11,5 +17,27 @@ public class TowerPlaceManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        PlaceTowerAction.Enable();
+        PlaceTowerAction.performed += OnPlaceTower;
+    }
+
+    private void OnDisable()
+    {
+        PlaceTowerAction.performed -= OnPlaceTower;
+        PlaceTowerAction.Disable();
+    }
+
+    public void StartPlacingTower (GameObject towerPrefab)
+    {
+
+    }
+
+    private void OnPlaceTower(InputAction.CallbackContext context)
+    {
+
     }
 }
