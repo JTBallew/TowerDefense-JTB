66e3813ca36d256865e982e3d7f31346fe5d5473
JTBallew <jtballew06@gmai.com>
2025-08-07 19:17:24 -0500

    Added Catapult Tower

---
+++ Assets/Scripts/Tower/CatapultTower.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class CatapultTower : Tower
+{
+    [SerializeField] private GameObject boulderPrefab;
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FireAt(Enemy target)
+    {
+        if (boulderPrefab != null)
+        {
+            GameObject projectileInstance = Instantiate(boulderPrefab, transform.position + new Vector3(0, 1.5f, 0), Quaternion.identity);
+            projectileInstance.GetComponent<Projectile>().SetTarget(target.transform);
+        }
+    }
+
+    protected override Enemy GetTargetEnemy()
+    {
+        ClearDestroyedEnemies();
+
+        Enemy furthestEnemy = null;
+        float furthestDistance = float.MinValue;
+        foreach (Enemy enemy in enemiesInRange)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy > furthestDistance)
+            {
+                furthestDistance = distanceToEnemy;
+                furthestEnemy = enemy;
+            }
+        }
+        return furthestEnemy;
+    }
+}
